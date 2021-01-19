7813507f0711608abf268e9487635eec680a55f8
Harsh Agrawal <agrawalharsh199925@gmail.com>
2021-01-19 11:50:11 +0530

    binary_to_decimal

---
+++ C++/binary_to_decimal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using std::cout;
+using std::cin;
+int main(){
+    int n,r,dn=0,i=0;
+    cout<<"Enter any Binary number\n";
+    cin>>n;
+    while(n>0){
+        r=n%10;
+        n=n/10;
+        dn=dn+r*pow(2,i);  
+        i++;
+        }
+    cout<<"Converted binary number is "<<dn;
+    return 0;
+}
