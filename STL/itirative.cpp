fe6cc154e466a90c48e8e0c7e94b634eb5034b4e
Harsh Agrawal <agrawalharsh199925@gmail.com>
2021-05-05 16:41:29 +0530

    vectors

--- STL/itirative.cpp
+++ STL/itirative.cpp
@@ -2,6 +2,67 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+
+void RotateArray(vector<int>& vec){
+        int low = 0;
+        int high = vec.size()-1;
+        int temp;
+        while(low<high){
+            temp=vec[low];
+            vec[low]=vec[high];
+            vec[high]=temp;
+            low++;
+            high--;
+        }
+        cout<<"\n";
+        for(int i=0;i<vec.size();i++){
+		cout<<vec[i]<<" ";
+	}
+
+}
+void LinearSearch(vector<int>&vec,int ele){
+    bool flag=false;
+    for(int i=0;i<vec.size();i++){
+        if(vec[i] == ele){
+            flag=true;
+            break;
+        }
+    }
+    cout<<"\n";
+    if(flag){
+        cout<<"Found"<<"\n";
+    }
+    else{
+        cout<<"Not found"<<"\n";
+    }
+}
+void BinarySearch(vector<int>&vec,int ele){
+    int low=0;
+    int high=vec.size()-1;
+    bool flag=false;
+    while(low<=high){
+         int mid=(low+high)/2;
+        if(vec[mid] == ele){
+            flag =true;
+            break;
+        }
+        else if(vec[mid]>ele){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+     cout<<"\n";
+    if(flag){
+        cout<<"Found"<<"\n";
+    }
+    else{
+        cout<<"Not found"<<"\n";
+    }
+  
+}
+
 int main(){
 	vector<int>vec;
 	vec.push_back(100);
@@ -21,4 +82,9 @@ int main(){
 		cout<<vec[i]<<" ";
 	}
 
+    RotateArray(vec);
+    LinearSearch(vec,100);
+    BinarySearch(vec,90);
+
+    return 0;
 }
